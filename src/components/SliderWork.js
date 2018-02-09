802dcfda490d618755312036f8afcf5f6283a113
marek parafianowicz <marek.parafianowicz@netguru.co>
2018-02-09 12:51:08 +0100

    Create checkbox state

--- src/components/SliderWork.js
+++ src/components/SliderWork.js
@@ -2,36 +2,42 @@ import React, { Component } from 'react';
 import Slider from 'rc-slider';
 
 class SliderWork extends React.Component {
-  keys(arr) {
-    var a = 100/3;
-    var b = -1;
-    
-    arr = [];
-    
-    while(b++, b<= 3){
-      arr.push(a*b);
-    }
-    return arr
-    console.log(arr)
+  keys(workInput) {
+    const length = workInput.length - 1
+    const delta = 100/length;
+    const numericKeys = workInput.map((_, i) => {
+      return delta*i
+    })
+    return numericKeys
   }
 
   marksObject(workInput) {
-    // const keys = [0, 33, 66, 100];
     const marks = {};
-    this.keys(this.props.arr).forEach((key, i) => marks[key] = workInput[i]);
+    this.keys(workInput).forEach((key, i) => marks[key] = workInput[i]);
     return marks
   }
+  
 
   style = { width: 400, margin: 50 };
 
+
   render() {  
+    
     
     return (
       <div className='Work'>
         <form>
-        <div style={this.style}>
-          <Slider min={0} marks={this.marksObject(this.props.workInput)} step={null} included={false} defaultValue={20} />
-        </div>
+          <div type='level' style={this.style}>
+            <Slider 
+              name='workInput'
+              min={0} 
+              marks={this.marksObject(this.props.workInput)} 
+              step={null} 
+              included={false} 
+              defaultValue={this.props.defaultValue} 
+              onChange={this.props.onChange} 
+            />
+          </div>
         </form>
       </div>
     );
