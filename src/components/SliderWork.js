27deaa6dd534e79c7715da833dffe763362be5b6
marek parafianowicz <marek.parafianowicz@netguru.co>
2018-02-09 14:16:21 +0100

    Create Slider state

--- src/components/SliderWork.js
+++ src/components/SliderWork.js
@@ -2,27 +2,9 @@ import React, { Component } from 'react';
 import Slider from 'rc-slider';
 
 class SliderWork extends React.Component {
-  keys(workInput) {
-    const length = workInput.length - 1
-    const delta = 100/length;
-    const numericKeys = workInput.map((_, i) => {
-      return delta*i
-    })
-    return numericKeys
-  }
-
-  marksObject(workInput) {
-    const marks = {};
-    this.keys(workInput).forEach((key, i) => marks[key] = workInput[i]);
-    return marks
-  }
-  
-
   style = { width: 400, margin: 50 };
 
-
   render() {  
-    
     
     return (
       <div className='Work'>
@@ -31,7 +13,7 @@ class SliderWork extends React.Component {
             <Slider 
               name='workInput'
               min={0} 
-              marks={this.marksObject(this.props.workInput)} 
+              marks={this.props.marks}
               step={null} 
               included={false} 
               defaultValue={this.props.defaultValue} 
